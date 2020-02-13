b20960546735ebf44d7e19e243eb45bc94f1b062
hidayoshi <toto0818summer@gmail.com>
2020-02-13 13:10:02 +0900

    非同期処理をするため、コードの整形をした

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,26 +2,26 @@
 
 class LikesController < ApplicationController
   def create
-    # @like = Like.new(user_id: current_user.id)
-    # @like.save
-    # @like_count = Like.where(user_id: current_user.id).count
-    # byebug
     @micropost = Micropost.find(params[:micropost_id])
     @micropost.like(current_user, params[:micropost_id])
-    respond_to do |format|
-      format.html { redirect_to root_path }
-      format.js
-    end
-    # redirect_to root_path
+    @micropost.reload
+
+    # respond_to do |format|
+    #   format.html { redirect_to root_path }
+    #   format.js
+    # end
+    redirect_to root_path
   end
 
   def destroy
     @micropost = Like.find(params[:id]).micropost
     @micropost.unlike(current_user, @micropost.id)
-    respond_to do |format|
-      format.html { redirect_to root_path }
-      format.js
-    end
-    # redirect_to root_path
+    @micropost.reload
+
+    # respond_to do |format|
+    #   format.html { redirect_to root_path }
+    #   format.js
+    # end
+    redirect_to root_path
   end
 end
