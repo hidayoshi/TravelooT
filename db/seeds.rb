9634a14e0787aa4e01d04cbb1f0d0f362f7788be
hidayoshi <toto0818summer@gmail.com>
2020-02-16 10:13:56 +0900

    seedファイルを更新

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-4.times do |n|
+10.times do |n|
   name = "name#{n + 1}"
   email = "user#{n + 1}@user.com"
   password = 'password'
@@ -22,8 +22,20 @@
   user.save!
 end
 
-users = User.order(:created_at).take(2)
+users = User.order(:created_at).take(4)
 3.times do
   description = Faker::Lorem.sentence(5)
-  users.each { |user| user.microposts.create!(description: description, image: open('#{Rails.root}/public/uploads/micropost/image/example.jpg')) }
+  users.each do |user|
+    user.microposts.create!(
+      description: description,
+      image: open('./public/images/example.jpg')
+    )
+  end
 end
+
+microposts = Micropost.all
+user = User.first
+microposts.each { |micropost| micropost.like(user, micropost.id)}
+# users = User.all
+# like_users = users[2..7]
+# like_users.each { |user| user.}
